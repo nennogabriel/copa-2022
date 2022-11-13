898d1cbf99ecfb5f883921fba208acb99d4a3b83
Pedro Moreno <nennogabriel@gmail.com>
2022-11-13 09:15:04 -0300

    fisrt commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen min-w-screen bg-yellow-300">
+      <Head>
+        <title>Lemonade: Bolão da copa</title>
+        <meta name="description" content="appzinho simples" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="">hello</main>
+    </div>
+  );
+}
